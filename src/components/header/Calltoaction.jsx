9e6e26c617388d94b14ff4d9b5bd43db55d87e4b
yuwei-3206 <yw829@drexel.edu>
2024-03-08 13:07:25 -0500

    update header

--- src/components/header/Calltoaction.jsx
+++ src/components/header/Calltoaction.jsx
@@ -1,14 +1,15 @@
 import React from "react";
-import CV from "../../assets/Yuwei Wu_Resume 2024.pdf"
+import CV from "../../assets/Yu-Wei Wu_Resume 2024.pdf"
 
 const Calltoaction = () => {
   const openCV = () => {
     window.open(CV, '_blank');
   };
+  
   return (
     <div className="header_btns">
       <div className="btn" onClick={openCV}>Resume</div>
-      <div className="btn" href="#contact">Get in Touch</div>
+      <a className="btn" href="#contact">Get in Touch</a>
     </div>
   );
 };
